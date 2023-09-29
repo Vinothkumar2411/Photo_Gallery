28e0684c5254c6b68e390f3c294ce5a2fc39de57
Vinothkumar <115887272+Vinothkumar2411@users.noreply.github.com>
2023-09-29 16:54:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AppBar from "./components/AppBar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <AppBar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
